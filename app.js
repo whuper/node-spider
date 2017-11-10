66b2b9024895ae77356d1c4f77593bd99d45bcac
wangwh <wangwenh@ngarihealth.com>
2017-11-10 17:45:39 +0800

    1110

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
 var cheerio = require('cheerio');
 var superagent = require('superagent');
 
+// extend with Request#proxy()
+require('superagent-proxy')(request);
+
+// HTTP, HTTPS, or SOCKS proxy to use
+var proxy = process.env.http_proxy || 'http://168.63.43.102:3128';
+
+
 var index_url = 'http://www.tokyo-hot.com/product/?page=';
 var cur_page = 1;
 
-superagent.get(index_url + cur_page).end(function(err, sres) {
-	if (err) {
-            console.log(err);
-	}
-        console.log(sres);
-        return;
-	var $ = cheerio.load(sres.text);
-	var items = [];
-	$('ul.list li.detail').each(function(idx, element) {
-		var $element = $(element);
-        var a_element = $element.find('a.rm');
-        var desc_element = a_element.find('.description2');
-		items.push({
-			href: a_element.attr('href'),
-			cover: a_element.find('>img').attr('src'),
-			title: desc_elementfind('.title').text(),
-			actor: desc_element.find('.actor').text(),
-			text: desc_element.find('.text').text()
-		});
+superagent
+	.get(index_url + cur_page)
+	.proxy(proxy)
+	.end(onresponse);
+
+
+
+function onresponse (err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res.status, res.headers);
+    console.log(res.body);
+			//console.log(res);
+			//return;
+		var $ = cheerio.load(res.text);
+		var items = [];
+		$('ul.list li.detail').each(function(idx, element) {
+			var $element = $(element);
+			var a_element = $element.find('a.rm');
+			var desc_element = a_element.find('.description2');
+			items.push({
+				href: a_element.attr('href'),
+				cover: a_element.find('>img').attr('src'),
+				title: desc_elementfind('.title').text(),
+				actor: desc_element.find('.actor').text(),
+				text: desc_element.find('.text').text()
+			});
 	});
 
 	//保存到数据库里
@@ -43,6 +59,6 @@ superagent.get(index_url + cur_page).end(function(err, sres) {
 	});
 
 	db.close();
-
-});
+  }
+}
 
